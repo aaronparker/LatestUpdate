6a131c418db2f0166585124353002ac0bcbd8ef6
Aaron Parker <aaron@stealthpuppy.com>
2018-04-17 10:26:41 +1000

    Update Pester tests

--- tests/PublicFunctions.Tests.ps1
+++ tests/PublicFunctions.Tests.ps1
@@ -25,33 +25,33 @@ Describe 'Get-LatestUpdate' {
     Context "Windows 10: Returns expected results with -SearchString" {
         It "Given '-Architecture x86' returns updates for Windows 10 x86" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Architecture x86
-            $Updates[0].Note -match "Cumulative.*x86" | Should -Be $True
+            $Updates.Note -match "Cumulative.*x86" | Should -Not -BeNullOrEmpty
         }
         It "Given '-Build 16299 -Architecture x64' returns updates for Windows Server 1709" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build 16299 -Architecture x64
-            $Updates[0].Note -match "Cumulative.*Server.*x64" | Should -Be $True
+            $Updates.Note -match "Cumulative.*Server.*x64" | Should -Be $True
         }
     }
     Context "Windows 10: Returns expected results with -Build" {
         It "Given '16299' returns updates for build 1709" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build '16299'
-            $Updates[0].Note -match "Cumulative.*1709" | Should -Be $True
+            $Updates.Note -match "Cumulative.*1709" | Should -Not -BeNullOrEmpty
         }
         It "Given '15063' returns updates for build 1703" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build '15063'
-            $Updates[0].Note -match "Cumulative.*1703" | Should -Be $True
+            $Updates.Note -match "Cumulative.*1703" | Should -Not -BeNullOrEmpty
         }
         It "Given '14393' returns updates for build 1607" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build '14393'
-            $Updates[0].Note -match "Cumulative.*1607" | Should -Be $True
+            $Updates.Note -match "Cumulative.*1607" | Should -Not -BeNullOrEmpty
         }
         It "Given '10586' returns updates for build 1511" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build '10586'
-            $Updates[0].Note -match "Cumulative.*1511" | Should -Be $True
+            $Updates.Note -match "Cumulative.*1511" | Should -Not -BeNullOrEmpty
         }
         It "Given '10240' returns updates for build 1507" {
             $Updates = Get-LatestUpdate -WindowsVersion Windows10 -Build '10240'
-            $Updates[0].Note -match "Cumulative.*1507" | Should -Be $True
+            $Updates.Note -match "Cumulative.*1507" | Should -Not -BeNullOrEmpty
         }
     }
 }
