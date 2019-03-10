615fbab34b1ccb3097dd4dd2f3a972a6fb0c8e54
Aaron Parker <aaron@stealthpuppy.com>
2019-03-11 00:23:59 +1100

    Update New-MdtPackagesFolder.ps1
    Update logic to cater for multiple paths (e.g. parent\child) to be created

--- LatestUpdate/Private/New-MdtPackagesFolder.ps1
+++ LatestUpdate/Private/New-MdtPackagesFolder.ps1
@@ -1,17 +1,17 @@
 Function New-MdtPackagesFolder {
     <#
-    .SYNOPSIS
-        Creates a folder in the MDT Packages node.
+        .SYNOPSIS
+            Creates a folder in the MDT Packages node.
 
-    .NOTES
-        Author: Aaron Parker
-        Twitter: @stealthpuppy
-    
-    .PARAMETER Drive
-        An existing PS drive letter mapped to an MDT deployment share.
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+        
+        .PARAMETER Drive
+            An existing PS drive letter mapped to an MDT deployment share.
 
-    .PARAMETER Path
-        A new folder to create below the Packages node in the MDT deployment share.
+        .PARAMETER Path
+            A new folder to create below the Packages node in the MDT deployment share.
     #>
     [CmdletBinding(SupportsShouldProcess = $True)]
     Param (
@@ -24,16 +24,25 @@ Function New-MdtPackagesFolder {
 
     # If $Path does not exist, attempt to create it
     If (!(Test-Path -Path "$($Drive):\Packages\$Path" -Type 'Container')) {
-        If ($pscmdlet.ShouldProcess("$($Drive):\Packages\$Path", "Creating")) {
-            Write-Verbose "Creating folder $($Drive):\Packages\$($Path)."
-            Try {
-                New-Item -Path "$($Drive):\Packages" -Enable "True" -Name $Path `
-                    -Comments "Created by 'New-MdtPackagesFolder'" `
-                    -ItemType "Folder"
+
+        # If path with multiple folders specified, create each one
+        $folders = $Path -split "\\"
+        $parent = "$($Drive):\Packages"
+        
+        ForEach ($folder in $folders) {
+            If (!(Test-Path -Path "$parent\$folder" -Type 'Container')) {
+                If ($pscmdlet.ShouldProcess("$parent\$folder", "Creating")) {
+                    Try {
+                        New-Item -Path $parent -Enable "True" -Name $folder `
+                            -Comments "Created by 'New-MdtPackagesFolder'" `
+                            -ItemType "Folder"
+                    }
+                    Catch {
+                        Throw "Failed to create Packages folder."
+                    }
+                }
             }
-            Catch {
-                Throw "Failed to create Packages folder."
-            }
+            $parent = "$parent\$folder"
 
             # Return status from New-Item
             Write-Output $?
