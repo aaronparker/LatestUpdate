5753d52ecda5d461055bb1b6dbf02cf1f04d987e
Aaron Parker <aaron@stealthpuppy.com>
2019-06-06 15:55:44 +1000

    Pester test - Get-UpdateFeed

--- tests/PrivateFunctions.Tests.ps1
+++ tests/PrivateFunctions.Tests.ps1
@@ -37,7 +37,9 @@ InModuleScope LatestUpdate {
     Describe 'Get-UpdateFeed' {
         . (Join-Path $modulePrivate "Get-ModuleResource.ps1")
         . (Join-Path $modulePrivate "Get-UpdateFeed.ps1")
-        $resourceStrings = Get-ModuleResource -Path (Join-Path $moduleParent "$module.json")
+        $Path = Join-Path $moduleParent "LatestUpdate.json"
+        Write-Host "JSON path: $Path."
+        $resourceStrings = Get-ModuleResource -Path $Path
         $updateFeed = Get-UpdateFeed -Uri $resourceStrings.UpdateFeeds.Windows10
         Context "Tests that Get-UpdateFeed returns valid XML" {
             It "Returns valid XML" {
