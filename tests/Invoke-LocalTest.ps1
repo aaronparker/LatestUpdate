c5924e8c45f6d961938dbc59aebf1c34c0d8147c
Aaron Parker <aaron@stealthpuppy.com>
2019-08-25 20:46:53 +1000

    Update scripts

--- tests/Invoke-LocalTest.ps1
+++ tests/Invoke-LocalTest.ps1
@@ -6,6 +6,6 @@
 Param ()
 
 # Invoke Pester tests and upload results to AppVeyor
-$res = Invoke-Pester -Path (Join-Path -Path $PWD -ChildPath "PublicFunctions.Tests.ps1") -OutputFormat NUnitXml -PassThru -ExcludeTag "AppVeyor"
+$res = Invoke-Pester -Path (Join-Path -Path $PWD -ChildPath "PublicFunctions.Tests.ps1") -PassThru -ExcludeTag "AppVeyor"
 If ($res.FailedCount -gt 0) { Throw "$($res.FailedCount) tests failed." }
 Write-Host ""
