d491eeaa84c9fc7d1ab556e1d701ace86b7e38fd
Fonta <gianni@fonta.nl>
2019-09-02 19:22:41 +0200

    Added SupportsShouldProcess to Set-Proxy

--- LatestUpdate/Private/Set-Proxy.ps1
+++ LatestUpdate/Private/Set-Proxy.ps1
@@ -1,10 +1,10 @@
 Function Set-Proxy {
     <#
         .SYNOPSIS
-            Adds a property to a PSObject by querying another property
+            Sets proxy config for the session
     #>
     [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
     Param (
         [Parameter(Mandatory = $False)]
         [System.String] $Proxy,
@@ -13,32 +13,54 @@ Function Set-Proxy {
         [System.Management.Automation.PSCredential] $ProxyCredential
     )
 
-    If ($Proxy) {
-        try {
-            $proxyUri = new-object System.Uri($Proxy)
+    Begin {
+        if (-not $PSBoundParameters.ContainsKey('Verbose')) {
+            $VerbosePreference = $PSCmdlet.SessionState.PSVariable.GetValue('VerbosePreference')
         }
-        catch {
-            Write-Warning -Message "$($MyInvocation.MyCommand): Failed to convert proxy URI to system URI."
-            Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
-            Throw $_.Exception.Message
+        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+            $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
         }
-
-        try {
-            [System.Net.WebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy ($proxyUri, $true)
+        if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+            $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
         }
-        catch {
-            Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
-            Throw $_.Exception.Message
-        }
+        Write-Verbose ('[{0}] Confirm={1} ConfirmPreference={2} WhatIf={3} WhatIfPreference={4}' -f $MyInvocation.MyCommand, $Confirm, $ConfirmPreference, $WhatIf, $WhatIfPreference)
     }
 
-    If ($ProxyCredential -ne [System.Management.Automation.PSCredential]::Empty) {
-        try {
-            [System.Net.WebRequest]::DefaultWebProxy.Credentials = $ProxyCredential
+    Process {
+        If ($Proxy) {
+            try {
+                $proxyUri = new-object System.Uri($Proxy)
+            }
+            catch {
+                Write-Warning -Message "$($MyInvocation.MyCommand): Failed to convert proxy URI to system URI."
+                Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
+                Throw $_.Exception.Message
+            }
+    
+            try {
+                if ($PSCmdlet.ShouldProcess("Configure proxy server?")) {
+                    Write-Verbose "Setting proxy server to $Proxy"
+                    [System.Net.WebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy ($proxyUri, $true)
+                }
+                
+            }
+            catch {
+                Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
+                Throw $_.Exception.Message
+            }
         }
-        catch {
-            Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
-            Throw $_.Exception.Message
+    
+        If ($ProxyCredential -ne [System.Management.Automation.PSCredential]::Empty) {
+            try {
+                if ($PSCmdlet.ShouldProcess("Configure proxy credentials?")) {
+                    Write-Verbose "Configuring proxy credentials"
+                    [System.Net.WebRequest]::DefaultWebProxy.Credentials = $ProxyCredential
+                }
+            }
+            catch {
+                Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
+                Throw $_.Exception.Message
+            }
         }
     }
 }
